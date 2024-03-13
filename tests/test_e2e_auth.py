6bfa191ac562d320732f4aa49b692aaf9f8179c0
Anastasia-Makarova <140186425+Anastasia-Makarova@users.noreply.github.com>
2024-03-13 22:30:51 +0100

    +1

--- tests/test_e2e_auth.py
+++ tests/test_e2e_auth.py
@@ -1,9 +1,12 @@
 from unittest.mock import Mock
 import json
 
+from requests import HTTPError
+
 import pytest
 from sqlalchemy import select
 from tests.conftest import TestingSessionLocal, user_data
+from fastapi import HTTPException
 
 from src.entity.models import User
 from src.schemas.user import UserSchema, RequestEmail
@@ -34,19 +37,16 @@ def test_signup(client, monkeypatch):
 
 
 
-# def test_repeat_signup(client, monkeypatch, mock_send_email):
-    # monkeypatch.setattr("src.routres.auth.send_email", mock_send_email)
-    # print('line1')
-    # response = client.post("api/auth/signup", json=user_data)
-    # print('line2')
-    # assert response.status_code == 409, response.text
-    # print('line3')
-    # data = response.json()
-    # print('line4')
-    # assert data["detail"] == messages.ACCOUNT_EXIST
-    # print('line5')
-    # assert not mock_send_email.called
-    # print('done')
+def test_repeat_signup(client, monkeypatch, mock_send_email, capsys):
+    try:
+        monkeypatch.setattr("src.routres.auth.send_email", mock_send_email)
+        with pytest.raises(HTTPException) as exc_info:
+            client.post("api/auth/signup", json=user_data)
+    except Exception as err:
+        captured = capsys.readouterr()
+        stdout_output = captured.out
+        assert f"409: {messages.ACCOUNT_EXIST}" in stdout_output
+
 
 
 # def test_not_confirmed_login(client):
